770e727b2f5420609f923d8b6e2a80aabc9342af
gourcetools <120996278+gourcetools@users.noreply.github.com>
2023-01-21 19:31:02 +0100

    0.0.1

---
+++ src/scripts/crawl-and-reply/crawl-events.sh
@@ -0,0 +1,33 @@
+if [ -d "node_modules" ]; then
+  echo "node_modules folder exists, skipping install"
+else
+  echo "node_modules folder does not exist"
+  echo "npm i puppeteer"
+  npm i puppeteer
+fi
+
+
+
+while true; do
+	PRIVKEY=$(cat ../../../config/REPLY-PRIVKEY)
+	REPLYMESSAGE=$(cat ../../../config/REPLY-MESSAGE)
+	ASKINTERVAL=$(cat ../../../config/ASK-INTERVAL)
+	# Save NOTE to OLDNOTE variable before checking note, so 
+	# we can compare them later to see if there is a new event id.
+ 	OLDNOTE=$NOTE
+ 	# Unset NOTE variable
+	unset NOTE
+	echo " == ❓Asking nostr.band for new messages..."
+	# Check for new messages using extract-value.js
+	NOTE=$(node crawl-events.js)
+		if [ "$NOTE" == "$OLDNOTE" ]; then   	
+        	echo " == ⌛ No new messages... checking again in 5 seconds"
+		echo " "
+		else
+		echo " == 🆕 New messages found, let's reply"
+		nostril --envelope --sec "$PRIVKEY" --content "$REPLYMESSAGE" --tag e $NOTE --tag e $NOTE | websocat wss://relay.nostr.info 
+		echo " == ⌛Checking for new messages in $ASKINTERVAL seconds..."
+		echo " "
+	fi
+sleep $ASKINTERVAL
+done
